f0489b9285f2ef0996d334d5cc5ac7181df8c42a
britniangela <66388266+britniangela@users.noreply.github.com>
2020-07-25 16:17:02 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
-let apiKey = "80f827f1223261e54bfb7371ee94cdd0";
-let city = "Orlando";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "80f827f1223261e54bfb7371ee94cdd0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Orlando");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
