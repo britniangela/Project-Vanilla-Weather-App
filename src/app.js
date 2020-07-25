50e91504eb50463fb0b7d275c3b6a132b88b68a7
britniangela <66388266+britniangela@users.noreply.github.com>
2020-07-25 14:50:52 -0400

    Adding js to icon

--- src/app.js
+++ src/app.js
@@ -23,21 +23,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/04n@2px.png`
+  );
 }
 
 let apiKey = "80f827f1223261e54bfb7371ee94cdd0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${apiKey}&units=metric`;
+let city = "Orlando";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
