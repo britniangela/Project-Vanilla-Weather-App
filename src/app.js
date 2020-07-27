a46642a8341ceeae3bd3ae1bd82dd9010290305b
britniangela <66388266+britniangela@users.noreply.github.com>
2020-07-26 20:57:06 -0400

    Added the forecast

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -19,7 +12,21 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -46,10 +53,39 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", `response.data.weather[0].description`);
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col-2">
+              <h3>
+                ${formatHours(forecast.dt * 1000)}
+              </h3>
+              <img
+                src="http://openweathermap.org/img/wn/${
+                  forecast.weather[0].icon
+                }@2x.png"
+                
+              />
+              <div class="weather-forecast-temperature">
+                <strong>${Math.round(
+                  forecast.main.temp_max
+                )}°</strong> ${Math.round(forecast.main.temp_min)}°
+              </div>
+            </div>`;
+  }
+}
+
 function search(city) {
   let apiKey = "80f827f1223261e54bfb7371ee94cdd0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
